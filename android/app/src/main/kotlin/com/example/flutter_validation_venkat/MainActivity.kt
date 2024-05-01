8ef8a2d9409378b8124a4b205b3a7713bbb14152
LENOVO-S <venkateshkrishnan255.@gmail.com>
2024-05-01 19:54:43 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_validation_venkat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_validation_venkat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/validation.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class TextValidationPage extends StatefulWidget {
+  const TextValidationPage({super.key});
+
+  @override
+  State<TextValidationPage> createState() => _TextValidationPageState();
+}
+
+class _TextValidationPageState extends State<TextValidationPage> {
+  final TextEditingController _NameController = TextEditingController();
+  final TextEditingController _MobileController = TextEditingController();
+
+  bool _validationName = false;
+  bool _validationMobile = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Validation"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _NameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                labelText: 'Name',
+                hintText: 'Enter Name',
+                errorText: _validationName ? "Name can't Be Empty " : null,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              controller: _MobileController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                labelText: 'Mobile',
+                hintText: 'Enter Number',
+                errorText: _validationMobile ? "Number can't Be Empty " : null,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue,
+              ),
+              onPressed: () {
+                setState(() {
+                  _validationName = _NameController.text.isEmpty;
+                  _validationMobile = _MobileController.text.isEmpty;
+                  if (!_validationName && !_validationMobile) {
+                    print('--------------->Save Button Clicked');
+                    _save();
+                  }
+                });
+
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _save() async {
+    print('--------------->Save Method');
+    print('------------------> Name : ${_NameController.text}');
+    print('------------------>Mobile No : ${_MobileController.text}');
+  }
+}
